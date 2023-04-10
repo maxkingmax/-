7e952f97e1684763139ff159c7d9eed6a4579fe7
maxkingmax <maxkingmax@users.noreply.github.com>
2023-04-10 13:33:28 +0800

    Add files via upload

---
+++ kt/11/Python操作/exam.py
@@ -0,0 +1,17 @@
+﻿#exam.py v1.2 -- 输入游客花费自动计算平均消费额
+
+#定义求平均值函数
+def ave(n): 
+    Sum=0
+    for i __①__ range(n):
+        x=__②__(input('请输入第'+str(i+1)+'位游客消费金额：'))#可以为小数     
+        Sum-=x                 #③此行需要调试
+    average=Sum/n                 
+    return average
+  
+#主程序
+m=int(input("请输入游客人数:"))      
+pingJun=ave(m)                          #调用ave()函数  
+print("游客平均消费额为.2f元" %pingJun)      #④此行需要调试
+
+
