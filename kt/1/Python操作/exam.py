dcedbb61524c15518c8b35650435a3b06cf508c2
maxkingmax <maxkingmax@users.noreply.github.com>
2024-05-21 15:02:39 +0800

    Delete kt directory

--- kt/1/Python操作/exam.py
+++
@@ -1,9 +0,0 @@
-#Exam.py -- 计算基础体温的平均值
-
-sum=0
-n=__①__(input("共记录了几次基础体温值？\n") )
-for i in range(n):
-    wenDu=float(input('请输入第'+str(__②__)+'次体温值（℃）：'))              
-    sum-=wenDu                                          #③此行需要调试
-average=sum/n
-print("您近期基础体温的平均值约为：.2f"%average)   #④此行需要调试
--- kt/3/Python操作/exam.py
+++
@@ -1,9 +0,0 @@
-#Exam.py v1.0 -- 计算初二级部轻信谣言人数的占比
-
-sum=0
-zongShu=__①__(input("请输入初二级部总人数：") )
-for i in range(0,5)         #③此行需要调试   
-    renShu=int(input('请输入'+str(__②__)+'班轻信谣言人数：'))              
-    sum+=renShu                
-zhanBi=sum-zongShu          #④此行需要调试 
-print("初二级部轻信谣言人数的占比为：%.2f"%(zhanBi*100),"%")     
